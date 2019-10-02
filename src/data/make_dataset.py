f9b132d8a94c828f4fbe107c0eec7b2bad92e4e4
anacriel <anacriel@protonmail.com>
2019-10-03 00:52:30 +0300

    Refactor the code and fix notebooks

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -10,6 +10,15 @@ from glob import glob
 
 
 def get_images(input_path, height=200, width=200, per_class=0):
+    """ Reads images from input_path and up-/down- scales them to specified size
+        Parameters:
+            input_path –– path to images
+            height/width –– size to up-/down- scale
+            per_class -– number of images per class you want to get
+        Returns:
+            images –– images list
+            labels –– labels names list
+    """
     images = []
     labels = []
     for class_dir_name in os.listdir(input_path):
@@ -30,7 +39,33 @@ def get_images(input_path, height=200, width=200, per_class=0):
     return images, labels
 
 
-def create_dataset_added_features(images, labels, kind):
+def prepare_images(input_path, output_path, height=200, width=200):
+    """ Reads images, resizes and writes them to the output path
+        Parameters:
+            input_path –– path to images
+            output_path –– path to processed images to save
+    """
+    for class_dir_name in os.listdir(input_path):  # total images per class
+        # handle macOS special directory
+        if '.DS_Store' != class_dir_name:
+            class_dir_path = os.path.join(input_path, class_dir_name)
+            for image_path in glob(os.path.join(class_dir_path, "*.png")):
+                image_bgr = cv2.imread(image_path, cv2.IMREAD_COLOR)
+                resized_img = cv2.resize(image_bgr, (height, width))
+                segmented_img = vs.segment_plant(resized_img)
+                cv2.imwrite(output_path + image_path[len(input_path):], segmented_img)
+
+
+def create_dataset(images, labels, kind):
+    """ Extracts features from images
+        Parameters:
+            images –– images to extract features from
+            labels –– images classes
+            kind –– name of the first data frame column (usually 'label' or 'title')
+        Returns:
+            df –– data frame with extracted features
+     """
+
     features_names = [kind, 'area', 'largest_area', 'number_of_elems', 'perimeter',
                       'aspect_ratio', 'circularity', 'mean_r', 'mean_g', 'mean_b',
                       'stddev_r', 'stddev_g', 'stddev_b',
@@ -38,7 +73,7 @@ def create_dataset_added_features(images, labels, kind):
     df = pd.DataFrame([], columns=features_names)
     for i in range(len(images)):
         # Delete background
-        masked_img, mask = vs.segment_plant(images[i])
+        masked_img, mask = vs.segment_plant(images[i], get_mask=True)
         img = cv2.cvtColor(masked_img, cv2.COLOR_BGR2RGB)
         gs = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
@@ -55,7 +90,7 @@ def create_dataset_added_features(images, labels, kind):
 
         x, y, w, h = cv2.boundingRect(cnt)
         aspect_ratio = 0.0 if h == 0.0 else float(w) / h
-        #rectangularity = 0.0 if area == 0.0 else w * h / area
+        # rectangularity = 0.0 if area == 0.0 else w * h / area
         circularity = 0.0 if area == 0.0 else (perimeter ** 2) / area
 
         # Color features
@@ -71,9 +106,9 @@ def create_dataset_added_features(images, labels, kind):
         green_mean = np.mean(green_channel_plant)
         blue_mean = np.mean(blue_channel_plant)
 
-        #red_med = np.median(red_channel_plant)
-        #green_med = np.median(green_channel_plant)
-        #blue_med = np.median(blue_channel_plant)
+        # red_med = np.median(red_channel_plant)
+        # green_med = np.median(green_channel_plant)
+        # blue_med = np.median(blue_channel_plant)
 
         red_std = np.std(red_channel_plant)
         green_std = np.std(green_channel_plant)
@@ -95,17 +130,6 @@ def create_dataset_added_features(images, labels, kind):
         df_temp = pd.DataFrame([vector], columns=features_names)
         df = df.append(df_temp)
     return df
-
-
-def prepare_images(input_path, output_path, height=200, width=200):
-    for class_dir_name in os.listdir(input_path):  # total images per class
-        if '.DS_Store' != class_dir_name:
-            class_dir_path = os.path.join(input_path, class_dir_name)
-            for image_path in glob(os.path.join(class_dir_path, "*.png")):
-                image_bgr = cv2.imread(image_path, cv2.IMREAD_COLOR)
-                resized_img = cv2.resize(image_bgr, (height, width))
-                segmented_img = vs.segment_plant(resized_img)
-                cv2.imwrite(output_path + image_path[len(input_path):], segmented_img)
 
 
 def increase_brightness(image, value=30):
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -26,37 +26,6 @@ def apply_threshold(matrix, low_value, high_value):
     return matrix
 
 
-def simplest_cb(img, percent):
-    half_percent = percent / 200.0
-
-    channels = cv2.split(img)
-
-    out_channels = []
-    for channel in channels:
-        assert len(channel.shape) == 2
-        # find the low and high precentile values (based on the input percentile)
-        height, width = channel.shape
-        vec_size = width * height
-        flat = channel.reshape(vec_size)
-
-        assert len(flat.shape) == 1
-
-        flat = np.sort(flat)
-
-        n_cols = flat.shape[0]
-
-        low_val  = flat[int(math.floor(n_cols * half_percent))]
-        high_val = flat[int(math.ceil( n_cols * (1.0 - half_percent)))]
-
-        # saturate below the low percentile and above the high percentile
-        thresholded = apply_threshold(channel, low_val, high_val)
-        # scale the channel
-        normalized = cv2.normalize(thresholded, thresholded.copy(), 0, 255, cv2.NORM_MINMAX)
-        out_channels.append(normalized)
-
-    return cv2.merge(out_channels)
-
-
 def read_images(path, data_type='train'):
     total = 0
     if data_type == 'train':
@@ -91,27 +60,31 @@ def read_images(path, data_type='train'):
         return images, titles
 
 
-def plot_raw_grid(images, labels, cols=12):
+def plot_raw_grid(images, cols=12):
     fig = plt.figure(1, (8, 8))
     grid = ImageGrid(fig, 111,  # similar to subplot(111)
                      nrows_ncols=(cols, cols),
-                     axes_pad=0.05)
-    n = 0
-    for str_i in range(cols):
-        for col_i in range(cols):
-            ax = grid[n]
-            if col_i == 0:
-                ax.text(-1000, 112, labels[n], verticalalignment='center')
-            img = images[n]
-            img, mask = segment_plant(img)
-            img = img[:, :, ::-1]
-            ax.imshow(img)
+                     axes_pad=0.05,  # pad between axes in inch.
+                     )
+
+    plt.rcParams['font.size'] = 8
+    k = 0
+    for label, val in images.items():
+        n = 0
+        for i in range(cols):
+            ax = grid[k]
+            if i == 0:
+                ax.text(-1000, 112, label, verticalalignment='center')
+            img = images[label][n]
+            resized_img = cv2.resize(img, (240, 240))
+            ax.imshow(resized_img)
             ax.axis('off')
-            str_i = str_i + 1
             n = n + 1
+            k = k + 1
+    plt.show()
 
 
-def plot_in_line(images, labels, cols=3):
+def plot_in_line(images, cols=3):
     fig, axs = plt.subplots(1, 3, figsize=(9, 3))
 
     for col_i in range(cols):
@@ -127,8 +100,8 @@ def create_mask_for_plant(image):
 
     mask = cv2.inRange(hsv, lower_green, upper_green)
 
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    closed_mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    # closed_mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
     imglab = morphology.label(mask)  # create labels in segmented image
     cleaned = morphology.remove_small_objects(imglab, min_size=64, connectivity=1)
@@ -139,12 +112,26 @@ def create_mask_for_plant(image):
     return img3
 
 
-def segment_plant(image):
+def segment_labeled_plants(images, sharpen=False):
+    seg_images = {}
+    for label, val in images.items():
+        seg_images[label] = []
+        for n in range(len(images[label])):
+            seg_image = segment_plant(images[label][n])
+            if sharpen:
+                seg_image = sharpen_image(seg_image)
+
+            seg_images[label].append(seg_image)
+
+    return seg_images
+
+
+def segment_plant(image, get_mask=False):
     mask = create_mask_for_plant(image)
     masked_img = cv2.bitwise_and(image, image, mask=mask)
     masked_img[mask == 0] = 255
 
-    return masked_img, mask
+    return masked_img, mask if get_mask else masked_img
 
 
 def sharpen_image(image):
